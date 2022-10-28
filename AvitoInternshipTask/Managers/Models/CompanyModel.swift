a30e3dc623229ee98a0f1f3a34ea08e701f38a87
Fett00 <musaew2014@gmail.com>
2022-10-28 16:03:03 +0300

    add codable models

---
+++ AvitoInternshipTask/Managers/Models/CompanyModel.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+struct CompanyModel: Codable {
+
+    let company: CompanyChildModel
+}
+
+struct CompanyChildModel: Codable {
+
+    let name: String
+    let employees: EmployeeModel
+}
+
+struct EmployeeModel: Codable {
+
+    let name: String
+    let phoneNumber: String
+    let skills: [String]
+}
