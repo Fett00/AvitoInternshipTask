f58aea1d86d6e5390ee116391cf9dd0c2a312e60
Fett00 <musaew2014@gmail.com>
2022-10-28 22:42:15 +0300

    add parser

---
+++ AvitoInternshipTask/Managers/Parse/ParseManager.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+protocol ParseManagerProtocol: AnyObject {
+
+    func parse<ModelType: Decodable>(type: ModelType.Type, data: Data) -> ModelType?
+}
+
+class ParseManager: ParseManagerProtocol {
+
+    private let jsonParser: JSONDecoder
+
+    init(jsonParser: JSONDecoder = JSONDecoder()) {
+        self.jsonParser = jsonParser
+        jsonParser.keyDecodingStrategy = .convertFromSnakeCase
+    }
+
+    func parse<ModelType: Decodable>(type: ModelType.Type, data: Data) -> ModelType? {
+
+        do {
+            let model = try jsonParser.decode(ModelType.self, from: data)
+            return model
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+}
